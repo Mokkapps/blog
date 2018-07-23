cabbcd3327e75cb87c52ec1862980c8e8e328657
Michael Hoffmann <michael.hoffmann@jambit.com>
2018-07-23 09:10:33 +0200

    Use new Gatsby blog

---
+++ config/SiteConfig.js
@@ -0,0 +1,23 @@
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+
+  siteTitle: 'Mokkapps Blog', // Navigation and Site Title
+  siteTitleAlt: 'Mokkapps Blog', // Alternative Site title for SEO
+  siteUrl: 'https://www.mokkapps.de/blog', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteBanner: '/social/banner.jpg',
+  siteDescription: 'Blog from Michael Hoffmann (Mokkapps) about software development',
+
+  siteFBAppID: '123456789', // Facebook App ID
+  userTwitter: '@Mokkapps', // Twitter Username
+  ogSiteName: 'minimal', // Facebook Site Name
+
+  // Manifest and Progress color
+  themeColor: '#3498DB',
+  backgroundColor: '#2b2e3c',
+
+  // Settings for typography.js
+  headerFontFamily: 'Bitter',
+  bodyFontFamily: 'Open Sans',
+  baseFontSize: '18px',
+};
---
+++ src/pages/index.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import styled from 'styled-components';
+import MdHome from 'react-icons/lib/md/home';
+import MdEmail from 'react-icons/lib/md/email';
+
+import Layout from '../components/Layout';
+import Article from '../components/Article';
+import Wrapper from '../components/Wrapper';
+import Button from '../components/Button';
+import SectionTitle from '../components/SectionTitle';
+
+import { media } from '../utils/media';
+
+const Content = styled.div`
+  grid-column: 2;
+  box-shadow: 0 4px 120px rgba(0, 0, 0, 0.1);
+  background: ${props => props.theme.white};
+  border-radius: 1rem;
+  padding: 3rem 6rem;
+  @media ${media.tablet} {
+    padding: 3rem 2rem;
+  }
+  @media ${media.phone} {
+    padding: 2rem 1.5rem;
+  }
+  overflow: hidden;
+`;
+
+const Hero = styled.div`
+  grid-column: 2;
+  padding: 3rem 2rem 6rem 2rem;
+  text-shadow: 0 12px 30px rgba(0, 0, 0, 0.15);
+  color: ${props => props.theme.white};
+
+  h1 {
+    color: ${props => props.theme.white};
+  }
+
+  p {
+    font-size: 1.68rem;
+    margin-top: -1rem;
+    @media ${media.phone} {
+      font-size: 1.25rem;
+    }
+    @media ${media.tablet} {
+      font-size: 1.45rem;
+    }
+  }
+`;
+
+const WebsiteLink = styled.a`
+  margin-right: 1rem;
+`;
+
+const IndexPage = props => {
+  const postEdges = props.data.allMarkdownRemark.edges;
+
+  return (
+    <Layout>
+      <Wrapper>
+        <Hero>
+          <h1>Hi.</h1>
+          <p>I&apos;m Michael Hoffmann and I publish my private software projects under the pseudonym Mokkapps.</p>
+          <br />
+          <p>Mobile, game & frontend developer. Blogger. Videogame enthusiast. Beekeeper.</p>
+          <WebsiteLink href="https://www.mokkapps.de">
+            <Button big>
+              <MdHome/>
+              Mokkapps website
+            </Button>
+          </WebsiteLink>
+          <Link to="/contact">
+            <Button big>
+              <MdEmail/>
+              Contact
+            </Button>
+          </Link>
+        </Hero>
+        <Content>
+          <SectionTitle>Latest stories</SectionTitle>
+          {postEdges.map(post => (
+            <Article
+              title={post.node.frontmatter.title}
+              date={post.node.frontmatter.date}
+              excerpt={post.node.excerpt}
+              timeToRead={post.node.timeToRead}
+              slug={post.node.fields.slug}
+              category={post.node.frontmatter.category}
+              key={post.node.fields.slug}
+            />
+          ))}
+        </Content>
+      </Wrapper>
+    </Layout>
+  );
+};
+
+export default IndexPage;
+
+/* eslint no-undef: off */
+export const IndexQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            date(formatString: "DD.MM.YYYY")
+            category
+          }
+          excerpt(pruneLength: 200)
+          timeToRead
+        }
+      }
+    }
+  }
+`;
