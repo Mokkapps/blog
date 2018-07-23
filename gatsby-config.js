a63d64067e9b6754cab0435647206dd243a055f3
Michael Hoffmann <kontakt@mokkapps.de>
2018-07-23 20:46:10 +0200

    Added favicon and google analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,50 +1,50 @@
-const config = require('./config/SiteConfig');
+const config = require("./config/SiteConfig");
 
-const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix;
+const pathPrefix = config.pathPrefix === "/" ? "" : config.pathPrefix;
 
 module.exports = {
   pathPrefix: config.pathPrefix,
   siteMetadata: {
     siteUrl: config.siteUrl + pathPrefix,
-    buildTime: new Date(Date.now()).toLocaleString(),
+    buildTime: new Date(Date.now()).toLocaleString()
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-styled-components',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-styled-components",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'post',
-        path: `${__dirname}/blog`,
-      },
+        name: "post",
+        path: `${__dirname}/blog`
+      }
     },
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-external-links',
+            resolve: "gatsby-remark-external-links",
             options: {
-              target: '_blank',
-              rel: 'nofollow noopener noreferrer',
-            },
+              target: "_blank",
+              rel: "nofollow noopener noreferrer"
+            }
           },
-          'gatsby-remark-prismjs',
-          'gatsby-remark-autolink-headers',
-        ],
-      },
+          "gatsby-remark-prismjs",
+          "gatsby-remark-autolink-headers"
+        ]
+      }
     },
     {
-      resolve: 'gatsby-plugin-typography',
+      resolve: "gatsby-plugin-typography",
       options: {
-        pathToConfigModule: 'src/utils/typography.js',
-      },
+        pathToConfigModule: "src/utils/typography.js"
+      }
     },
-    'gatsby-plugin-catch-links',
-    'gatsby-plugin-sitemap',
-    'gatsby-plugin-lodash',
+    "gatsby-plugin-catch-links",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-lodash",
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-plugin-manifest",
       options: {
         name: config.siteTitle,
         short_name: config.siteTitleAlt,
@@ -52,10 +52,24 @@ module.exports = {
         start_url: config.pathPrefix,
         background_color: config.backgroundColor,
         theme_color: config.themeColor,
-        display: 'fullscreen',
-        icon: 'src/favicon.png',
-      },
+        display: "fullscreen",
+        icon: "src/favicon.png"
+      }
     },
-    'gatsby-plugin-offline',
-  ],
+    "gatsby-plugin-offline",
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-93160141-2 ",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"]
+      }
+    }
+  ]
 };
